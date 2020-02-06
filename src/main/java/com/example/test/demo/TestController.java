6062bd8693ce41d83c26644a4f650f8374d115a3
Admin <sohailtechnoark>
2020-02-06 11:53:00 +0530

    demo

---
+++ src/main/java/com/example/test/demo/TestController.java
@@ -0,0 +1,19 @@
+package com.example.test.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("test")
+public class TestController {
+
+
+    @GetMapping
+    private String test() {
+
+
+        return "Test Successful.";
+    }
+
+}
